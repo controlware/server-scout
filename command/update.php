ae03e5eeff74a70eaac22ca1fc37e8f433e7ebad
Murilo Feres <muriloferes90@gmail.com>
2021-08-18 22:55:26 -0300

    ...

--- command/update.php
+++ command/update.php
@@ -2,6 +2,8 @@
 
 require_once(__DIR__."/../default/handling.php");
 
+Log::write("Iniciando atualização do Server Scout.");
+
 $command = implode("; ", [
     "cd ".__DIR__."/..",
     "git fetch --all",
@@ -11,4 +13,8 @@ $command = implode("; ", [
 Shell::execute($command);
 
 $result = Shell::execute("cd ".__DIR__."/..; git pull");
-var_dump($result);
+if($result["output"] === "Already up-to-date."){
+    Log::write("Atualizado com sucesso!");
+}else{
+    Log::write("Houve uma falha na atualização.");
+}
